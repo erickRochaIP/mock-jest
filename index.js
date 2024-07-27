2eb6fc13f7bea4407bbbdd99eb0a839c68657345
Erick Rocha <erickfr1011@gmail.com>
2024-07-27 16:38:47 -0300

    feat(meow-facts): Consumir API que retorna fatos aleatorios de gatos.

---
+++ index.js
@@ -0,0 +1,7 @@
+const { getMeowFact } = require("./meow-facts");
+
+run();
+
+async function run(){
+    console.log(await getMeowFact({ upperCase: true }));
+}
---
+++ meow-facts.js
@@ -0,0 +1,13 @@
+function requestMeowFact() {
+    return fetch("https://meowfacts.herokuapp.com/?count=1")
+        .then(res => res.json())
+}
+
+function getMeowFact({ upperCase = false }) {
+    return requestMeowFact()
+        .then(json => json.data)
+        .then(data => data[0])
+        .then(fact => upperCase ? fact.toUpperCase() : fact.toLowerCase());
+}
+
+module.exports = { getMeowFact }
