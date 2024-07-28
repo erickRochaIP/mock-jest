f5cce4b2a61a5d67de7535f184f8fb2500cc65d6
Erick Rocha <erickfr1011@gmail.com>
2024-07-27 21:13:56 -0300

    test(require-rewire): Implementar rewire nos testes.

--- meow-facts.test.js
+++ meow-facts.test.js
@@ -1,15 +1,16 @@
-const { getMeowFact } = require("./meow-facts");
+const rewire = require("rewire");
+const meowFacts = rewire("./meow-facts");
 
 describe("meow-facts suite", () => {
 
     test("Returns a meow fact to lower case", async () => {
-        const fact = await getMeowFact({ upperCase: false });
+        const fact = await meowFacts.getMeowFact({ upperCase: false });
 
         expect(fact).toEqual(fact.toLowerCase());
     })
 
     test("Returns a meow fact to upper case", async () => {
-        const fact = await getMeowFact({ upperCase: true });
+        const fact = await meowFacts.getMeowFact({ upperCase: true });
 
         expect(fact).toEqual(fact.toUpperCase());
     })
