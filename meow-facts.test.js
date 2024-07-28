68ab8c5650f66fb70f3e9ca6851fc7719a263b6d
Erick Rocha <erickfr1011@gmail.com>
2024-07-27 21:06:59 -0300

    test(mock-return): Criar mocks para evitar chamar a API.

--- meow-facts.test.js
+++ meow-facts.test.js
@@ -1,16 +1,24 @@
-const { getMeowFact } = require("./meow-facts");
+const rewire = require("rewire");
+const meowFacts = rewire("./meow-facts");
+
+const meowFactMocked = "When well treated, a cat can live twenty or more years but the average life span of a domestic cat is 14 years.";
+
+const requestMeowFactMock = jest.fn(() => Promise.resolve({
+    data: [meowFactMocked]
+}));
+meowFacts.__set__('requestMeowFact', requestMeowFactMock);
 
 describe("meow-facts suite", () => {
 
     test("Returns a meow fact to lower case", async () => {
-        const fact = await getMeowFact({ upperCase: false });
+        const fact = await meowFacts.getMeowFact({ upperCase: false });
 
-        expect(fact).toEqual(fact.toLowerCase());
+        expect(fact).toEqual(meowFactMocked);
     })
 
     test("Returns a meow fact to upper case", async () => {
-        const fact = await getMeowFact({ upperCase: true });
+        const fact = await meowFacts.getMeowFact({ upperCase: true });
 
-        expect(fact).toEqual(fact.toUpperCase());
+        expect(fact).toEqual(meowFactMocked.toUpperCase());
     })
 })
